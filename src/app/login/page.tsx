f92235c99ceeeeed766956deaac432f7ed417a6f
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-10-02 15:12:22 +0530

    upto db connection (46:46)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>This will be a login page!</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+    return (
+        <div>
+            <h1>This will be a signup page!</h1>
+        </div>
+    )
+}
