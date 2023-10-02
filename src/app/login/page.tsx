b2aef7ad265412302cfc5d8a5909ad4dc0123cac
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-10-02 21:12:23 +0530

    signup and login basic ui (1:01:36)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function LoginPage() {
-    return (
-        <div>
-            <h1>This will be a login page!</h1>
-        </div>
-    )
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login to your account</h1>
+      <hr />
+      <p className="m-2">
+        <input
+          type="text"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="email"
+        />
+      </p>
+      <p className="m-2">
+        <input
+          type="password"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="password"
+        />
+      </p>
+      <p className="m-2">
+        Don't have an account? <Link href='/signup'>Signup</Link>
+      </p>
+      <p className="m-3">
+        <button onClick={onLogin}>Login</button>
+      </p>
+    </div>
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,55 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignupPage() {
-    return (
-        <div>
-            <h1>This will be a signup page!</h1>
-        </div>
-    )
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Create your account</h1>
+      <hr />
+      <p className="m-2">
+        <input
+          type="text"
+          id="username"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="username"
+        />
+      </p>
+      <p className="m-2">
+        <input
+          type="text"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="email"
+        />
+      </p>
+      <p className="m-2">
+        <input
+          type="password"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="password"
+        />
+      </p>
+      <p className="m-2">
+        Already have an account? <Link href='/login'>Login</Link>
+      </p>
+      <p className="m-3">
+        <button onClick={onSignup}>Signup</button>
+      </p>
+    </div>
+  );
 }
