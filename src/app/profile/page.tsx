5b300391776dae3a830bd6389c47d93105fffa51
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-10-05 22:42:15 +0530

    logout implemented api + a logout ui component on /profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
-    return (
-        <div className="flex flex-col justify-center items-center min-h-screen py-2">
-            <h1 className="text-4xl m-2">Profile</h1>
-            <p>User Profile</p>
-        </div>
-    )
+    const router = useRouter();
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen py-2">
+      <h1 className="text-4xl m-2">Profile</h1>
+      <p>User Profile</p>
+
+      <button onClick={logout} className="mt-3 border-2 p-2">Logout</button>
+    </div>
+  );
 }
