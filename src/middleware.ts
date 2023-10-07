e40d5b3caf24bdfe88f1109505ceb23c9b2b84bb
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-10-07 22:38:34 +0530

    middleware datatype update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
 
-    const isPublicPath = path === '/login' || path === '/signup';
+    const isPublicPath: boolean = path === '/login' || path === '/signup';
 
     const token = request.cookies.get('token')?.value || "";
 
